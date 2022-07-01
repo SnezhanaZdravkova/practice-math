2444daa7eb5a26cd6111d15f4f5b9809c0a94b0b
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-07-01 09:09:49 +0000

    Add changes to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
+// THe game variables 
 const gameElement = document.querySelector(".game")
 const ourForm = document.querySelector(".our-form")
 const ourField = document.querySelector(".our-field")
+const pointsNeeded = document.querySelector(".points-needed")
+const mistakesAllowed = document.querySelector(".mistakes-allowed")
+
 //initials results: correct and incorrect 
 let result = {
     score: 0,
     wrongAnswer: 0
 }
+/**
+ * The main game "loop", called when the script is first loaded and after the users answer has been processed 
+ */
 function updateGame(){
+    //this help us to store the result in memory 
     result.currentGame = generateGame();
     gameElement.innerHTML = `${result.currentGame.numOne} ${result.currentGame.operator} ${result.currentGame.numTwo}`;
     ourField.value = "";
@@ -15,14 +23,16 @@ function updateGame(){
 updateGame();
 //function - creates random number between 1 and 25
 function generateNumber(max){
-    return Math.floor(Math.random() * max + 1);
+    return Math.floor(Math.random() * max) + 1;
 }
-//returns random number one, random number two and random choice of the operator "+" or "-"
+/**
+ * returns random number one, random number two and random choice of the operator "+", "-", "x", "/"
+ */
 function generateGame(){
     return{
         numOne: generateNumber(25),
         numTwo: generateNumber(25),
-        operator: ["+", "-"][1]   
+        operator: ["+", "-", "x", "/"][generateNumber(3)]   
     }   
 }
 ourForm.addEventListener("submit", handleSubmit);
@@ -33,7 +43,26 @@ function handleSubmit(e){
     const b = result.currentGame;
     if(b.operator == "+"){
         correctAnswer = b.numOne + b.numTwo;
-    }else if(b.operator == "-"){
+    }
+    if(b.operator == "-"){
         correctAnswer = b.numOne - b.numTwo;
+    }
+    if(b.operator == "x"){
+        correctAnswer = b.numOne * b.numTwo;
+    }
+    if(b.operator == "/"){
+        //numOne *= numTwo;
+        correctAnswer = b.numOne / b.numTwo;
+    }
+    // this will compare the users answer with our correctAnswer
+    //The function parseInt in javaScript will make the value number if it is a string
+    if(parseInt(ourField.value, 25) === correctAnswer){
+        result.score++;
+        pointsNeeded.textContent = 10 - result.score;
+        updateGame();
+    }else{
+        result.wrongAnswer++;
+        mistakesAllowed.textContent = 3 - result.wrongAnswer;
+        updateGame();
     }
 }
