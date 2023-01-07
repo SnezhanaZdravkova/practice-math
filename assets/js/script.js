6f82b8856c63ca08cb1cd0b0e5559d7f0bf1b211
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2023-01-07 17:16:27 +0000

    Add function renderProgressBar

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const ourForm = document.querySelector(".our-form")
 const ourField = document.querySelector(".our-field")
 const pointsNeeded = document.querySelector(".points-needed")
 const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
 
 //initials results: correct and incorrect 
 let result = {
@@ -48,6 +49,7 @@ function handleSubmit(event){
         result.score++;
         pointsNeeded.textContent = 10 - result.score;
         updateGame();
+        renderProgressBar()
     }
     else {
         result.wrongAnswer++;
@@ -75,6 +77,11 @@ function resetGame(){
    result.wrongAnswer = 0;
    pointsNeeded.textContent = 10;
    mistakesAllowed.textContent = 2;
+   renderProgressBar()
+}
+
+function renderProgressBar(){
+    progressBar.style.transform = `scaleX(${result.score / 10})`
 }
 
 
